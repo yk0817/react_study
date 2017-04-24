424f24bb985d9d5643c757c2ff3c4f3b3da70b7e
山本憲 <em072029@gmail.com>
2017-04-24 22:56:11 +0900

    test

--- src/ch04/table.js
+++ src/ch04/table.js
@@ -1,3 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Excel = Rect.createClass({
+  displayName: 'Excel',
+  
+  propTypes: {
+    headers: React.PropTypes.arrayOf(
+      React.PropTypes.string
+    ),
+    initialData: React.PropTypes.arrayOf(
+      React.PropTypes.arrayOf(
+        React.PropTypes.string
+        
+      )
+    ),
+  },
+  
+})
